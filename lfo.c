70ff4fa2fcdc775b5c58d576ef158611a28237b2
Andreas Lindmark <kgs@Update.UU.SE>
2018-09-05 23:45:18 +0200

    En gång på nittiotalet

--- lfo.c
+++ lfo.c
@@ -4,6 +4,7 @@
 #include"orgel.h"
 
 #include"lfo.spec.c"
+#define _GNU_SOURCE
 
 typedef struct lfo_module{
   module;
@@ -15,32 +16,39 @@ static int32_t square(int time, int amp, int duty, int per){
   }
 
 static int32_t sine(int time, int amp, int per){
-  double tau = 2 * M_PI;
-  return amp*sin(tau/per);
+  return amp*sin((2.0*M_PI)/per);
+  }
+
+static int32_t triangle(int time, int amp, int per){
+  }
+
+static int32_t sawtooth(int time, int amp, int per){
   }
 
 static void tick(module *_m, int elapsed){
   lfo_module *m=(lfo_module *)_m;
   struct input_bundle *in=(struct input_bundle *)m->input.bundle.elements;
- 
   if(in->frequency.connection && in->amplitude.connection && in->waveform.connection){
     int32_t frequency=(in->frequency.connection->out_terminal.value.int32);
     int32_t amplitude=(in->amplitude.connection->out_terminal.value.int32);
     int32_t waveform=(in->waveform.connection->out_terminal.value.int32);
 
     // Rewrite the following
-    int32_t length = 1000000/frequency;
-    int32_t value = 0;
+    int32_t length=1000000/frequency;
+    int32_t value=0;
 
-    switch(wavform) {
-      case 0:
-        value = square(m->time, amplitude, length/2, length);
-        break;
+    switch(waveform) {
       case 1:
-        value = sine(m->time, amplitude, length);
+        value=triangle(m->time, amplitude, length);
+        break;
+      case 2:
+        value=sawtooth(m->time, amplitude, length);
+        break;
+      case 3:
+        value=square(m->time, amplitude, length/2, length);
         break;
       default:
-        value = square(m->time, amplitude, length/2, length);
+        value=sine(m->time, amplitude, length);
       }
     
     m->time+=elapsed;
