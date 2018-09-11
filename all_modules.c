4fb716543b48d5d94b81e2a4f3ac22ea6282c8f2
Euler <euler@euler.euler>
2018-09-11 04:28:23 +0200

    Hade råkat committa all_modules.c; tog nu bort den och lade till dess namn i .gitignore

--- all_modules.c
+++
@@ -1,2 +0,0 @@
-extern class mog_class, prio_class, na_class, sid_class, add_class, slew_class, lfo_class;
-class *all_classes[]={ &mog_class, &prio_class, &na_class, &sid_class, &add_class, &slew_class, &lfo_class,  0};
